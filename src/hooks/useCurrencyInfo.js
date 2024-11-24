167a84112e6afd8431ba474e160f59a43f450da2
saurav joshi <mesauravjoshii@gmail.com>
2024-11-24 13:12:31 +0530

    update nav

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -6,10 +6,23 @@ function useCurrencyInfo(currency){
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then((res) => res.json())
-        .then((res) => setData(res[currency]))
-        console.log(data);
+        .then((res) => {
+            // console.log(res);
+            setData(res[currency])
+        })
+        // console.log(res);
+
+        // const fetchData = async () => {
+        //     const response = await fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        //     const responseData = await response.json()
+        //     setData(responseData[currency])
+
+        //     console.log(responseData);
+        // }
+        // fetchData();
+
     }, [currency])
-    console.log(data);
+    // console.log(data);
     return data
 }
 
